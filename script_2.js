19f2dc08f9df4fa21dc3b2b33dc198f609f7072c
priyankarehan <41320547+priyankarehan@users.noreply.github.com>
2024-11-21 16:48:51 +0530

    Add files via upload

---
+++ script_2.js
@@ -0,0 +1,5 @@
+// Function to toggle the hamburger menu for small screens
+function toggleMenu(event) {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+}
